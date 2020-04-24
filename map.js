f854b0fdec20a60d25bddf7984c157eeb7dbba4f
Lily Donaldson <lilykdonaldson@gmail.com>
2020-04-24 13:40:53 -0500

    integrate map

--- map.js
+++ map.js
@@ -20,84 +20,84 @@
   var svg = d3.select(canvas).append("svg");
   var g = svg.append("g");
 
-  //Uploading data file, then() tells d3 to wait for file before proceeding with a callback function that manipulates the data
-  d3.csv('data/sample.csv')
-    .then(ready);
+//   //Uploading data file, then() tells d3 to wait for file before proceeding with a callback function that manipulates the data
+//   d3.csv('data/sample.csv')
+//     .then(ready);
 
-  function ready(nodes) { //Argument is name of the data returned from deferred function
+//   function ready(nodes) { //Argument is name of the data returned from deferred function
 
-    //Creates a circle svg mark for each of the power grid notes
-    nodes.forEach(function(d) {
-      d.LngLat = [+d.long, +d.lat];
-    });
+//     //Creates a circle svg mark for each of the power grid notes
+//     nodes.forEach(function(d) {
+//       d.LngLat = [+d.long, +d.lat];
+//     });
 
-    //Red circles with partial opacity
-    g.selectAll('circle')
-      .data(nodes)
-      .enter().append('circle')
-        .style("opacity", 0.6)
-        .style("fill", 'red')
-        .attr('r', 3)
-        .on("mouseover", handleMouseOver)
-        .on("mouseout", handleMouseOut);
+//     //Red circles with partial opacity
+//     g.selectAll('circle')
+//       .data(nodes)
+//       .enter().append('circle')
+//         .style("opacity", 0.6)
+//         .style("fill", 'red')
+//         .attr('r', 3)
+//         .on("mouseover", handleMouseOver)
+//         .on("mouseout", handleMouseOut);
 
-    //Ensuring the locations are set correctly and updated when the map view changes
-    //Event listeners for the map so that the circles are updated when the user interacts with the map
-    map.on("viewreset", function() {
-      updateNodes();
-    });
-    map.on("movestart", function() {
-      g.attr("visibility", "hidden");
-    });
-    map.on("moveend", function() {
-      updateNodes();
-      g.attr("visibility", "visible");
-    });
+//     //Ensuring the locations are set correctly and updated when the map view changes
+//     //Event listeners for the map so that the circles are updated when the user interacts with the map
+//     map.on("viewreset", function() {
+//       updateNodes();
+//     });
+//     map.on("movestart", function() {
+//       g.attr("visibility", "hidden");
+//     });
+//     map.on("moveend", function() {
+//       updateNodes();
+//       g.attr("visibility", "visible");
+//     });
 
-    updateNodes();
-} //end ready()
+//     updateNodes();
+// } //end ready()
 
-function updateNodes() { //Calls each time the map stops moving, then draws the circles in the updated pixel location
-  //Converting long and lat to the correct pixel locations on the screen
-  g.selectAll('circle')
-    .attr('cx', function(d) {return project(d.LngLat).x})
-    .attr('cy', function(d) {return project(d.LngLat).y});
-} //end updateNodes()
+// function updateNodes() { //Calls each time the map stops moving, then draws the circles in the updated pixel location
+//   //Converting long and lat to the correct pixel locations on the screen
+//   g.selectAll('circle')
+//     .attr('cx', function(d) {return project(d.LngLat).x})
+//     .attr('cy', function(d) {return project(d.LngLat).y});
+// } //end updateNodes()
 
-function project(d) {
-  return map.project(new mapboxgl.LngLat(+d[0], +d[1]));
-}
+// function project(d) {
+//   return map.project(new mapboxgl.LngLat(+d[0], +d[1]));
+// }
 
-/* FIXME: Hover
-Reference: http://bl.ocks.org/WilliamQLiu/76ae20060e19bf42d774
-*/
-//Create Event Handlers for mouse
-function handleMouseOver(d, i) {
+// /* FIXME: Hover
+// Reference: http://bl.ocks.org/WilliamQLiu/76ae20060e19bf42d774
+// */
+// //Create Event Handlers for mouse
+// function handleMouseOver(d, i) {
 
-  //Select element, change color and size
-  d3.select(this).attr({
-    fill: "orange",
-    r: radius * 2
-  });
+//   //Select element, change color and size
+//   d3.select(this).attr({
+//     fill: "orange",
+//     r: radius * 2
+//   });
 
-  //Specify where to put label of text
-  svg.append("text").attr({
-    id: "t" + d.x + "-" + d.y + "-" + i,  //Create an id for text so we can select it later for removing on mouseout
-    x: function() { return xScale(d.x) - 30; },
-    y: function() { return yScale(d.y) - 15; }
-  })
-  .text(function() {
-  return [d.x, d.y];  // Value of the text
-  });
-}
+//   //Specify where to put label of text
+//   svg.append("text").attr({
+//     id: "t" + d.x + "-" + d.y + "-" + i,  //Create an id for text so we can select it later for removing on mouseout
+//     x: function() { return xScale(d.x) - 30; },
+//     y: function() { return yScale(d.y) - 15; }
+//   })
+//   .text(function() {
+//   return [d.x, d.y];  // Value of the text
+//   });
+// }
 
-function handleMouseOut(d, i) {
-  //Select element, change color back to normal
-  d3.select(this).attr({
-    fill: "red",
-    r: radius
-  });
+// function handleMouseOut(d, i) {
+//   //Select element, change color back to normal
+//   d3.select(this).attr({
+//     fill: "red",
+//     r: radius
+//   });
 
-  //Select text by id and then remove
-  d3.select("#t" + d.x + "-" + d.y + "-" + i).remove();  // Remove text location
-}
+//   //Select text by id and then remove
+//   d3.select("#t" + d.x + "-" + d.y + "-" + i).remove();  // Remove text location
+// }
